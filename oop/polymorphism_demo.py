71d2bc1114110fd5a7d42d50eff0856a0dd9e8d0
Billy Och <ochiengbilly02@gmail.com>
2025-08-15 11:30:05 +0300

    Update: Squaring the radius

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -7,8 +7,7 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return math.pi * self.radius * self.radius
-
+        return math.pi * self.radius ** 2
 class Rectangle(Shape):
     def __init__(self, length, width):
         self.width = width
