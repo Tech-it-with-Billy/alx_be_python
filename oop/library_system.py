f0a6d2dabac87da3e968444cb86510b3d3aee884
Billy Och <ochiengbilly02@gmail.com>
2025-08-15 10:37:17 +0300

    Update: update the return functions of the book classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,21 +4,21 @@ class Book:
         self.author = author
         
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size = 0):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"{self.title} by {self.author}, file size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}kB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count = 0):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"{self.title} by {self.author}, {self.page_count} pages"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
